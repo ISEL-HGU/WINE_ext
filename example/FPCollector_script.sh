7da3242eee8da669760763d1d90dcf3c48276ca9
testation21 <uenho830@gmail.com>
2022-12-06 14:13:08 +0900

    add scripts

---
+++ example/FPCollector_script.sh
@@ -0,0 +1,1 @@
+./WINE_ext/bin/WINE_ext -f -o ./SAResultMiner_example.csv -t ./TargetAddress.txt
---
+++ example/SAResultMiner_script.sh
@@ -0,0 +1,1 @@
+./WINE_ext/bin/WINE_ext -s -R category/java/errorprone.xml/AssignmentInOperand -t ./TargetAddress.txt -p ./pmd-bin-6.25.0/bin/run.sh -o ./SAResultMiner_example.csv
